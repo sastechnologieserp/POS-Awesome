a8a6f94694354c4577a2d22ea1b52a802929e137
0muhammedirfan-pc-wsl <irfannadapuram@gmail.com>
2026-03-24 18:14:41 +0530

    chore(release): bump version to 15.3.40

--- posawesome/__init__.py
+++ posawesome/__init__.py
@@ -6,7 +6,7 @@ try:
 except ModuleNotFoundError:  # pragma: no cover - frappe may not be installed during setup
 	frappe = None
 
-__version__ = "15.3.4"
+__version__ = "15.3.40"
 
 
 def console(*data):
