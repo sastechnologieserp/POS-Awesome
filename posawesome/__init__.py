4ebe06391d3e9f52766a29d719224c807607259f
GitHub Action <action@github.com>
2026-05-05 06:39:21 +0000

    chore: bump version to 15.4.0

--- posawesome/__init__.py
+++ posawesome/__init__.py
@@ -6,7 +6,7 @@ try:
 except ModuleNotFoundError:  # pragma: no cover - frappe may not be installed during setup
 	frappe = None
 
-__version__ = "15.3.50"
+__version__ = "15.4.0"
 
 
 def console(*data):
